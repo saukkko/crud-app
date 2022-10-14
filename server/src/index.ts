94a54cd3ebbdf59e8b91fd29b9080ab00e29f4b9
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-10-14 19:50:19 +0300

    revert commit f71a1bc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,11 @@
 import express from "express";
 import { api } from "./Routes.js";
 
-const PORT = isNaN(Number(process.env.PORT)) ? 3001 : process.env.PORT;
+const PORT = isNaN(Number(process.env.PORT)) ? 53000 : Number(process.env.PORT);
 
 const app = express();
-process.env.NODE_ENV === "production"
-  ? app.use("/", express.static("client"))
-  : app.all("/", (req, res) => res.send("OK"));
 
 app.use("/api", api);
 app.all("*", (req, res) => res.send("hello"));
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, "localhost", () => console.log(`Listening on port ${PORT}`));
