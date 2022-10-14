dc30e10243a5374bdc61099e426a33dc9c52c881
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-10-14 20:30:13 +0300

    kill forked express if runfile exists

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-import { spawn } from "child_process";
+import { writeFile } from "fs/promises";
+import { spawn, exec } from "child_process";
 
-spawn("npm", ["start", "--prefix", "server"], {
+if (process.platform !== "linux") process.exit(1);
+
+exec("kill -SIGINT $(cat /var/run/express.pid)");
+const express = spawn("node", ["/app/server/index.js"], {
   detached: true,
 });
+
+writeFile("/var/run/express.pid", express.pid)
+  .catch(console.error)
+  .then(() => console.log(`express forked to background. pid: ${express.pid}`));
