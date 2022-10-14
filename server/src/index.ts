f71a1bcd29569c8a3451ab743210618fa5ad118f
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-10-14 17:54:07 +0300

    use static html content if on production

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,10 @@ import { api } from "./Routes.js";
 const PORT = isNaN(Number(process.env.PORT)) ? 3001 : process.env.PORT;
 
 const app = express();
+process.env.NODE_ENV === "production"
+  ? app.use("/", express.static("../client/"))
+  : app.all("/", (req, res) => res.send("OK"));
+
 app.use("/api", api);
 app.all("*", (req, res) => res.send("hello"));
 
