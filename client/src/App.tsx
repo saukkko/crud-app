717f0ec4a7846bd6ed19e1b35bb2aa09e8dc6ffc
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-10-17 22:42:35 +0300

    updated App.tsx to render the new form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,55 @@
-import React from "react";
-import { MouseEvent } from "react";
-export const App = () => {
+import React, { useState } from "react";
+import { Form } from "./Form";
+import type { ClassNames } from "./types/ClassNames";
+
+export const classNames: ClassNames = {
+  inputElement: ["text-sm rounded"],
+  inputLabel: ["text-md my-2 opacity-80"],
+  loginButton: [
+    "my-2",
+    "p-2",
+    "rounded-full",
+    "bg-slate-700/70",
+    "hover:bg-slate-700/80",
+    "outline-1",
+    "active:outline",
+    "active:ring",
+  ],
+};
+
+const TestComponent = () => {
+  const [data, setData] = useState<string>("");
+
+  const fetchData = async () => {
+    const x = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then((res) => res.text())
+      .then((x) => x);
+    setData(x);
+  };
+
+  const handleClick = () => fetchData();
+
   return (
     <>
-      <h1>sladfkjsadf</h1>
-      <div className="btn">
-        <Btn />
-      </div>
+      <button
+        onClick={handleClick}
+        className="rounded-full ml-2 py-1 px-5 bg-indigo-700/80 hover:bg-indigo-700/90 outline-1 active:outline active:ring"
+      >
+        Load data
+      </button>
+      <pre>{data}</pre>
     </>
   );
 };
 
-const handleClick = async (ev: MouseEvent<HTMLInputElement>) => {
-  const text = await (await fetch("/api/hello")).text();
-
-  console.log(text);
+export const App = () => {
+  return (
+    <div className="container-md mx-auto my-2">
+      <h1 className="m-1 p-1 text-xl uppercase font-bold">otsikko</h1>
+      <div>
+        <TestComponent />
+      </div>
+      <Form />
+    </div>
+  );
 };
-
-const Btn = () => (
-  <input type="button" onClick={handleClick} value="sdklfjhsdkj" />
-);
